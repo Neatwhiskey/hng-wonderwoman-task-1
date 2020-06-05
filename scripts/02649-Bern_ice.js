413a34d7fae63c3f613a52562851167f84422443
Bernice El. Omiunu <53863709+Beel234@users.noreply.github.com>
2020-06-05 23:39:19 +0100

    02649-Bern_ice

---
+++ scripts/02649-Bern_ice.js
@@ -0,0 +1,9 @@
+function hngTask(){
+    var fullname = "Bernice Omiunu"
+    var internship_id = "HNG-02649"
+    var language = "Javascript"
+    var email = "bel4luv4@gmail.com"
+    console.log("Hello World, this is " + fullname + " with HNGi7 ID " +  internship_id + " using " + language + " for stage 2 task " + email);
+}
+
+hngTask();
